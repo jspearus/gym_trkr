850517c0241cecadfbd6cff13733fa4fab51ad0a
jspearus <jspearus@gmail.com>
2023-04-12 22:37:21 +0000

    update

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,198 @@
+import * as React from 'react';
+import { useNavigate } from 'react-router-dom';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
+
+const pages = ['Home', 'Join Game', 'Create Game'];
+const settings = ['Profile', 'Account', 'Dashboard'];
+
+function ResponsiveAppBar({ setMenuOption }) {
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
+    const [page, setPage] = React.useState('');
+    const navigate = useNavigate();
+
+
+    const handleOpenNavMenu = (event) => {
+        setPage(event.target.innerText);
+        setMenuOption(event.target.innerText)
+        setAnchorElNav(event.currentTarget);
+
+    }
+
+    const handleCloseNavMenu = (event) => {
+        setPage(event.target.innerText);
+        setMenuOption(event.target.innerText)
+        setAnchorElNav(null);
+    };
+
+    const handleOpenUserMenu = (event) => {
+        setPage(event.target.innerText);
+        setAnchorElUser(event.currentTarget);
+    };
+
+
+    const handleCloseUserMenu = (event) => {
+        setPage(event.target.innerText);
+        setMenuOption(event.target.innerText)
+        setAnchorElUser(null);
+    };
+    React.useEffect(() => {
+        if (page != null) {
+            if (page.toLocaleLowerCase() === 'home') {
+
+                navigate('/');
+            }
+            else if (page.toLocaleLowerCase() === 'create game') {
+
+                navigate('/create');
+            }
+            else if (page.toLocaleLowerCase() === 'profile') {
+
+                navigate('/join');
+            }
+            else if (page.toLocaleLowerCase() === 'join game') {
+                if (localStorage.user == '') {
+                    navigate('/join');
+                }
+                else {
+                    navigate('/question');
+                }
+            }
+        }
+    }, [page, navigate]);
+    return (
+        <AppBar position="static">
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <QuestionMarkIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="a"
+                        href="/api/"
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'none', md: 'flex' },
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        inQUIZitive
+                    </Typography>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: 'block', md: 'none' },
+                            }}
+                        >
+                            {pages.map((page) => (
+                                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                                    <Typography textAlign="center">{page}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                    <QuestionMarkIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+                    <Typography
+                        variant="h5"
+                        noWrap
+                        component="a"
+                        href="/api/"
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'flex', md: 'none' },
+                            flexGrow: 1,
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        inQUIZitive
+                    </Typography>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                        {pages.map((page) => (
+                            <Button
+                                key={page}
+                                onClick={handleOpenNavMenu}
+                                sx={{ my: 2, color: 'white', display: 'block' }}
+                            >
+                                {page}
+                            </Button>
+                        ))}
+                    </Box>
+
+                    <Box sx={{ flexGrow: 0 }}>
+                        <Tooltip title="Open settings">
+                            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                                <Avatar alt="Jeff Spear" src="/static/logo192.png" />
+                            </IconButton>
+                        </Tooltip>
+                        <Menu
+                            sx={{ mt: '45px' }}
+                            id="menu-appbar"
+                            anchorEl={anchorElUser}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            open={Boolean(anchorElUser)}
+                            onClose={handleCloseUserMenu}
+                        >
+                            {settings.map((setting) => (
+                                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                                    <Typography textAlign="center">{setting}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
+}
+export default ResponsiveAppBar;
